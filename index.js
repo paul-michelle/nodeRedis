7d317e6f87789eff6991de445c9d8fbae620bcf2
paul-michelle <paulmichellegallery@gmail.com>
2021-12-16 00:24:27 +0300

    Add writing info into redis when requested

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const Redis = require('redis');
 
-const PORT = 3210
-const placeholdingUri = 'https://jsonplaceholder.typicode.com/photos'
+const PORT = 3210;
+const placeholdingUri = 'https://jsonplaceholder.typicode.com/photos';
+const EXPIRATION_TIME = 3600;
 
+const redisCli = Redis.createClient();
+redisCli.connect()
 const app = express();
 const crossOrigin = cors();
 app.use(crossOrigin);
+
 
 app.get(
     '/photos',
     async(req, res) => {
         const albumID = req.query.albumID;
-        const { data } = await axios.get(
-            placeholdingUri,                // 200 OK 1052 ms 870.84 KB
-            {params: {albumID}}
-            );  
+        const { data } = await axios.get(placeholdingUri, {params: {albumID}});  
+        dataToStoreInRedis = JSON.stringify(data);
+        redisCli.setEx('photos', EXPIRATION_TIME, dataToStoreInRedis);
         res.json(data);
-    }
-)
+        }
+);
 
 app.get(
     '/photos/:id',
     async(req, res) => {
         const singleItemUri = placeholdingUri + '/' + req.params.id;
-        console.log(singleItemUri)
         const { data } = await axios.get(singleItemUri);  // 200 OK 445 ms 448 B
         
         res.json(data);
     }
-)
+);
 
-app.listen(PORT)
+app.listen(PORT);
