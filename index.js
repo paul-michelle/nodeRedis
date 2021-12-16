1e9291d390fe6a177c2c8d47ffe378bce42f6f0b
paul-michelle <paulmichellegallery@gmail.com>
2021-12-16 10:12:27 +0300

    Add checking if data is already stored in Redis

--- index.js
+++ index.js
@@ -3,33 +3,38 @@ const axios = require('axios');
 const cors = require('cors');
 const Redis = require('redis');
 
-const PORT = 3210;
+const PORT = 3211;
 const placeholdingUri = 'https://jsonplaceholder.typicode.com/photos';
 const EXPIRATION_TIME = 3600;
 
 const redisCli = Redis.createClient();
-redisCli.connect()
+redisCli.connect();
+redisCli.on('error', (err) => {'Redis Client Error', err});
+
 const app = express();
 const crossOrigin = cors();
 app.use(crossOrigin);
-
+app.use(express.urlencoded({ extended: true}));
 
 app.get(
     '/photos',
-    async(req, res) => {
+    async (req, res) => {
         const albumID = req.query.albumID;
-        const { data } = await axios.get(placeholdingUri, {params: {albumID}});  
-        dataToStoreInRedis = JSON.stringify(data);
-        redisCli.setEx('photos', EXPIRATION_TIME, dataToStoreInRedis);
-        res.json(data);
+        const dataAlreadyStoredInRedis = await redisCli.get('photos');
+        if (dataAlreadyStoredInRedis) {
+            return res.json(JSON.parse(dataAlreadyStoredInRedis))
         }
-);
+        const { data: dataToStoreInRedis } = await axios.get(placeholdingUri, {params: {albumID}});
+        redisCli.setEx('photos', EXPIRATION_TIME, JSON.stringify(dataToStoreInRedis));
+        res.json(dataToStoreInRedis);
+    }
+    );
 
 app.get(
     '/photos/:id',
     async(req, res) => {
         const singleItemUri = placeholdingUri + '/' + req.params.id;
-        const { data } = await axios.get(singleItemUri);  // 200 OK 445 ms 448 B
+        const { data } = await axios.get(singleItemUri);
         
         res.json(data);
     }
